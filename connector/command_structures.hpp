0a47bec26b3b1e0094ab8ba30770ea49521c4f31
James Litton <litton@cs.umd.edu>
2014-06-17 15:56:22 -0400

    small modifications while talking with Neal

--- connector/command_structures.hpp
+++ connector/command_structures.hpp
@@ -7,19 +7,40 @@
 namespace ctrl {
 #endif
 
-#define COMMAND_GET_CXN 1
-#define COMMAND_CONNECT 2
-#define COMMAND_DISCONNECT 3
-#define COMMAND_GETADDR 4
+enum commands {
+	COMMAND_GET_CXN,
+	COMMAND_CONNECT,
+	COMMAND_DISCONNECT,
+	COMMAND_GETADDR 
+};
+
+const uint32_t BROADCAST_TARGET(~0);
+
+struct bitcoin_msg {
+	uint32_t length;
+	uint8_t payload[0];
+};
+
 
 struct command {
    uint32_t length;
+	uint32_t version;
    uint32_t command;
+	uint32_t message_id;
    /* still have to decide the format of target, as it depends on some
       data structure changes, but target will correspond to indices in
       the logs and values returned by COMMAND_GET_CXN */
+
    uint32_t targets[0]; 
 };
+
+/*
+  send_message(length, message) //returns message id
+*/
+
+
+
+
 
 
 
