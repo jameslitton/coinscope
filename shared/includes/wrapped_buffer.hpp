62006b936352ed93e74ad3b8d39c08af47b02204
James Litton <litton@cs.umd.edu>
2014-07-31 00:37:37 -0400

    auto-selecting buffer type

---
+++ shared/includes/wrapped_buffer.hpp
@@ -0,0 +1,153 @@
+#ifndef WRAPPED_BUFFER_HPP
+#define WRAPPED_BUFFER_HPP
+
+#include "mmap_buffer.hpp"
+#include "alloc_buffer.hpp"
+
+/* this uses alloc_buffer for small allocations and switched to an
+   mmap_buffer once allocations reach a certain size. Both are COW */
+
+/* it is important to note, this is a BUFFER, not a container. It can
+   be used as an allocator for containers. placement new/delete is not
+   called on objects. Itertor end() spans to the end of the allocated
+   space, not necessarily the end of the used space, though it does
+   work to maintain alignment so it can be used as an allocator */
+
+template <typename T>
+class wrapped_buffer {
+public:
+	typedef typename std::aligned_storage<sizeof(T),alignof(T)>::type POD_T;
+	typedef T value_type;
+	typedef typename std::size_t size_type;
+	typedef typename std::ptrdiff_t difference_type;
+	typedef value_type& reference;
+	typedef const value_type& const_reference;
+	typedef T* pointer;
+	typedef const T* const_pointer;
+	typedef const_pointer const_iterator;
+
+private:
+	mmap_buffer<T> *mbuffer_;
+	alloc_buffer<T> *abuffer_;
+
+
+	struct iterator { /* non-const iterator has to COW, so blech */
+		//pos is in elt strides 
+
+		typedef typename wrapped_buffer<T>::difference_type difference_type;
+		typedef typename wrapped_buffer<T>::value_type value_type;
+		typedef typename wrapped_buffer<T>::pointer pointer;
+		typedef typename wrapped_buffer<T>::reference reference;
+		typedef typename std::random_access_iterator_tag iterator_category;
+
+		iterator(wrapped_buffer *owner, size_type pos) : owner_(owner), pos_(pos) {}
+
+		iterator & operator++() { /* prefix inc */
+			++pos_;
+			return *this;
+		}
+
+		iterator operator+(const iterator &x) {
+			return iterator(owner_, pos_ + x.pos_);
+		}
+
+		iterator operator+(size_type x) {
+			return iterator(owner_, pos_ + x);
+		}
+
+		iterator operator-(size_type x) {
+			return iterator(owner_, pos_ - x);
+		}
+
+		difference_type operator-(const iterator &x) {
+			difference_type lhs(pos_);
+			difference_type rhs(x.pos_); 
+			return lhs - rhs;
+		}
+
+		iterator operator++(int) { //postfix
+			iterator rv(*this);
+			++(*this);
+			return rv;
+		}
+
+		iterator & operator--() { /* prefix dec */
+			assert(pos_ > 0);
+			--pos_;
+			return *this;
+		}
+
+		iterator operator--(int) {
+			iterator rv(*this);
+			--(*this);
+			return rv;
+		}
+
+		value_type & operator*() const {
+			return *(pointer)(owner_->ptr() + pos_);
+		}
+
+		value_type* operator->() const {
+			return (pointer)(owner_->ptr() + pos_);
+			
+		}
+
+		bool operator==(const iterator &other) const {
+			/* two iterators that share the same underlying buffer but have different owners are considered different */
+			return other.owner_ == owner_ &&
+				other.pos_ == pos_;
+		}
+
+		bool operator !=(const iterator &other) const {
+			return !(other == *this);
+		}
+
+	private:
+		wrapped_buffer *owner_;
+		size_type pos_;
+	};
+
+
+public:
+
+	wrapped_buffer();
+	wrapped_buffer(size_type initial_allocation);
+	wrapped_buffer(wrapped_buffer &copy);
+	wrapped_buffer(wrapped_buffer &&moved);
+	wrapped_buffer & operator=(wrapped_buffer other);
+	~wrapped_buffer();
+	iterator begin() {
+		return iterator(this, 0);
+	}
+
+	iterator end() {
+		return iterator(this, allocated() / sizeof(POD_T));
+	}
+
+	const_iterator cbegin() const {
+		return mbuffer_ ? mbuffer_->cbegin() : abuffer_->cbegin();
+	}
+
+	const_iterator cend() const {
+		return mbuffer_ ? mbuffer_->cend() : abuffer_->cend();
+	}
+
+	void realloc(size_type new_elt_cnt);
+
+	/* this doesn't act as a write, no copy made */
+	const_pointer const_ptr() const {
+		return mbuffer_ ? mbuffer_->const_ptr() : abuffer_->const_ptr();
+	}
+
+	/* This acts as a write. If refcount > 1, copy made first */
+	pointer ptr();
+
+	size_type allocated() const {
+		return mbuffer_ ? mbuffer_->allocated() : abuffer_->allocated();
+	}
+
+};
+
+
+
+#endif
---
+++ shared/src/wrapped_buffer.cpp
@@ -0,0 +1,120 @@
+#include <cstring>
+#include <unistd.h>
+
+#include <string>
+#include <stack>
+#include <map>
+#include <stdexcept>
+
+
+#include <cstdlib>
+
+#include "wrapped_buffer.hpp"
+
+using namespace std;
+
+#define MMAP_THRESHOLD 50000
+
+template <typename T> 
+wrapped_buffer<T>::wrapped_buffer() : wrapped_buffer(0) {}
+
+template <typename T> 
+wrapped_buffer<T>::wrapped_buffer(size_type initial_elements) 
+	: mbuffer_(nullptr),
+	  abuffer_(nullptr)
+{
+	size_t size = initial_elements * sizeof(POD_T);
+	if (size > MMAP_THRESHOLD) {
+		mbuffer_ = new mmap_buffer<T>(initial_elements);
+	} else {
+		abuffer_ = new alloc_buffer<T>(initial_elements);
+	}
+	if (mbuffer_ == nullptr && abuffer_ == nullptr) {
+		throw runtime_error(string("wrapped_buffer failure: ") + strerror(errno));
+	}
+}
+
+template <typename T> 
+wrapped_buffer<T>::wrapped_buffer(wrapped_buffer &copy) 
+	: mbuffer_(nullptr), abuffer_(nullptr)
+{
+	if (copy.mbuffer_) {
+		mbuffer_ = new mmap_buffer<T>(*copy.mbuffer_);
+	} else if (copy.abuffer_) {
+		abuffer_ = new alloc_buffer<T>(*copy.abuffer_);
+	} else {
+		assert(false);
+	}
+	
+}
+
+template <typename T> 
+wrapped_buffer<T>::wrapped_buffer(wrapped_buffer &&moved) 
+	: mbuffer_(moved.mbuffer_),
+	  abuffer_(moved.abuffer_)
+{
+	mbuffer_ = nullptr;
+	abuffer_ = nullptr;
+}
+
+template <typename T> 
+wrapped_buffer<T> & wrapped_buffer<T>::operator=(wrapped_buffer other) {
+	/* we swap with other. They'll decrement our refcount on desctruction */
+	std::swap(mbuffer_, other.mbuffer_);
+	std::swap(abuffer_, other.abuffer_);
+	return *this;
+}
+
+template <typename T> 
+wrapped_buffer<T>::~wrapped_buffer() {
+	delete mbuffer_;
+	delete abuffer_;
+}
+
+
+template <typename T> 
+void wrapped_buffer<T>::realloc(size_type new_elt_cnt) {
+	size_type size = new_elt_cnt * sizeof(POD_T);
+
+	if (size > MMAP_THRESHOLD) {
+
+		if (mbuffer_) {
+			mbuffer_->realloc(new_elt_cnt);
+		} else if (abuffer_) {
+			mbuffer_ = new mmap_buffer<T>(new_elt_cnt);
+			size_t to_copy = min(mbuffer_->allocated(), abuffer_->allocated());
+			memcpy(mbuffer_->ptr(), abuffer_->cbegin(), to_copy);
+			delete abuffer_;
+			abuffer_ = nullptr;
+		} else {
+			assert(false);
+		}
+
+	} else {
+
+		if (mbuffer_) {
+			abuffer_ = new alloc_buffer<T>(new_elt_cnt);
+			size_t to_copy = min(mbuffer_->allocated(), abuffer_->allocated());
+			memcpy(abuffer_->ptr(), mbuffer_->cbegin(), to_copy);
+			delete mbuffer_;
+			mbuffer_ = nullptr;
+		} else if (abuffer_){
+			abuffer_->realloc(new_elt_cnt);
+		} else {
+			assert(false);
+		}
+	}
+
+
+}
+
+template <typename T> 
+typename wrapped_buffer<T>::pointer wrapped_buffer<T>::ptr() {
+	return mbuffer_ ? mbuffer_->ptr() : abuffer_->ptr();
+}
+
+template class wrapped_buffer<uint8_t>;
+template class wrapped_buffer<uint32_t>;
+
+
+
