fd497c07b0e6d76af4f425a3f59380b8141efaa4
James Litton <litton@cs.umd.edu>
2014-08-05 22:52:29 -0400

    refactored out some get cxn stuff

--- clients/get_nodes.cpp
+++ clients/get_nodes.cpp
@@ -50,78 +50,53 @@ int main(int argc, char *argv[]) {
 
 	int sock = unix_sock_client((const char*)cfg->lookup("connector.control_path"), false);
 
-	uint32_t alloc_size = sizeof(struct message) + sizeof(struct command_msg);
-	struct message *msg = (struct message*) ::operator new(alloc_size);
-	bzero(msg, sizeof(*msg));
+	struct sockaddr_in *data(nullptr);
+	size_t data_len(0);
 
-	msg->version = 0;
-	msg->length = hton((uint32_t)sizeof(struct command_msg));
-	msg->message_type = COMMAND;
-	struct command_msg *cmsg = (struct command_msg*) &msg->payload;
-	bzero(cmsg, sizeof(*cmsg));
-	cmsg->command = COMMAND_GET_CXN;
+	std::function<std::function<void(struct connection_info *, size_t)>(size_t) > callgen(
+		        [&](size_t addresses) -> std::function<void(struct connection_info*, size_t)> {
+			        if (addresses) {
+				        size_t mult = 0;
+				        size_t page_size = sysconf(_SC_PAGE_SIZE);
 
-	do_write(sock, msg, alloc_size);
-	cout << "just wrote " << alloc_size << " bytes\n";
-
-	delete msg;
+				        while(++mult * page_size < addresses * sizeof(struct sockaddr_in));
 
 
-	uint32_t len;
-	if (recv(sock, &len, sizeof(len), MSG_WAITALL) != sizeof(len)) {
-		throw runtime_error(strerror(errno));
+				        char tmpname[] = "/tmp/nodelist-XXXXXX";
+				        int fd = mkstemp(tmpname);
+				        if (fd < 0) {
+					        throw runtime_error(strerror(errno));
+				        }
+
+				        if (ftruncate(fd, mult * page_size) != 0) {
+					        throw runtime_error(strerror(errno));
+				        }
+				        data = (struct sockaddr_in*) mmap(NULL, mult * page_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+				        close(fd);
+				        if (data == MAP_FAILED) {
+					        throw runtime_error(strerror(errno));
+				        }
+				        data_len = mult * page_size;
+				        return [data](struct connection_info *info, size_t elt) {
+					        cout << "handle_id: " << info->handle_id << endl;
+					        cout << "remote: " << *((struct sockaddr*)&info->remote_addr) << endl;
+					        cout << "local: " << *((struct sockaddr*)&info->local_addr) << endl;
+					        cout << "------------------------------------------------------------------------\n";
+					        memcpy(data+elt, &info->remote_addr, sizeof(info->remote_addr));					
+				        };
+			        } else {
+				        return [](struct connection_info *, size_t) { /* should never get called */ };
+			        }
+			
+		        });
+
+
+	get_all_cxn<std::function<std::function<void(struct connection_info *, size_t)>(size_t) >,  
+	            std::function<void(struct connection_info *, size_t)> >(sock, callgen);
+
+	if (data) {
+		munmap(data, data_len);
 	}
-	len = ntoh(len);
-
-	cout << "Got length of " << len << endl;
-
-	cout << "cisizeof " << sizeof(struct connection_info) << endl;
-	assert((len*1.0) / sizeof(struct connection_info) - (len / sizeof(struct connection_info)) == 0);
-	size_t addresses = len / sizeof(struct connection_info);
-	if (addresses == 0) {
-		cout << "no addresses\n";
-		return 0;
-	}
-
-	cout << "Expecting " << addresses << " addresses" << endl;
-
-	size_t mult = 0;
-	size_t page_size = sysconf(_SC_PAGE_SIZE);
-
-	while(++mult * page_size < addresses * sizeof(struct sockaddr_in));
-
-	char tmpname[] = "/tmp/nodelist-XXXXXX";
-	int fd = mkstemp(tmpname);
-	if (fd < 0) {
-		throw runtime_error(strerror(errno));
-	}
-
-	if (ftruncate(fd, mult * page_size) != 0) {
-		throw runtime_error(strerror(errno));
-	}
-	struct sockaddr_in *data = (struct sockaddr_in*) mmap(NULL, mult * page_size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
-	void * start = data;
-	close(fd);
-
-	while(len > 0) {
-		struct connection_info info;
-		cerr << "Doing recv for " << sizeof(info) << " bytes" << endl;
-		if (recv(sock, &info, sizeof(info), MSG_WAITALL) != sizeof(info)) {
-			throw runtime_error(strerror(errno));
-		} else {
-			cout << "handle_id: " << info.handle_id << endl;
-			cout << "remote: " << *((struct sockaddr*)&info.remote_addr) << endl;
-			cout << "local: " << *((struct sockaddr*)&info.local_addr) << endl;
-			cout << "------------------------------------------------------------------------\n";
-			memcpy(data++, &info.remote_addr, sizeof(info.remote_addr));
-		}
-		len -= sizeof(info);
-	}
-
-	munmap(start, mult*page_size);
 
 	close(sock);
-
-
-
 }
--- clients/lib.hpp
+++ clients/lib.hpp
@@ -6,6 +6,9 @@
 #include <cstdint>
 #include <unistd.h>
 #include <stdexcept>
+#include <functional>
+
+#include "network.hpp"
 #include "command_structures.hpp"
 
 /* blocking writer, but EINTR can happen */
@@ -22,6 +25,83 @@ inline void do_write(int fd, const void *ptr, size_t len) {
 			}
 		}
 	} while ( so_far < len);
+}
+
+
+
+/* assumes sock is blocking connection to control channel, returns
+   number of connections received */
+
+uint32_t get_all_cxn_prefix(int sock) {
+	uint32_t alloc_size = sizeof(struct ctrl::message) + sizeof(struct ctrl::command_msg);
+	struct ctrl::message *msg = (struct ctrl::message*) ::operator new(alloc_size);
+	bzero(msg, sizeof(*msg));
+
+	msg->version = 0;
+	msg->length = hton((uint32_t)sizeof(struct ctrl::command_msg));
+	msg->message_type = ctrl::COMMAND;
+	struct ctrl::command_msg *cmsg = (struct ctrl::command_msg*) &msg->payload;
+	bzero(cmsg, sizeof(*cmsg));
+	cmsg->command = ctrl::COMMAND_GET_CXN;
+
+	do_write(sock, msg, alloc_size);
+
+	delete msg;
+
+	uint32_t len;
+	if (recv(sock, &len, sizeof(len), MSG_WAITALL) != sizeof(len)) {
+		throw std::runtime_error(strerror(errno));
+	}
+	len = ntoh(len);
+	return len;
+}
+
+template <typename C>
+size_t get_all_cxn(int sock, C callback) {
+
+	uint32_t len = get_all_cxn_prefix(sock);
+	assert((len*1.0) / sizeof(struct ctrl::connection_info) - (len / sizeof(struct ctrl::connection_info)) == 0);
+	size_t addresses = len / sizeof(struct ctrl::connection_info);
+	if (addresses == 0) {
+		return 0;
+	}
+
+	size_t i = 0;
+	while(len > 0) {
+		struct ctrl::connection_info info;
+		if (recv(sock, &info, sizeof(info), MSG_WAITALL) != sizeof(info)) {
+			throw std::runtime_error(strerror(errno));
+		} else {
+			callback(&info, i++);
+			len -= sizeof(info);
+		}
+	}
+	return addresses;
+}
+
+
+
+template <typename Cgen, typename C>
+size_t get_all_cxn(int sock, Cgen callgen) {
+	uint32_t len = get_all_cxn_prefix(sock);
+	assert((len*1.0) / sizeof(struct ctrl::connection_info) - (len / sizeof(struct ctrl::connection_info)) == 0);
+	size_t addresses = len / sizeof(struct ctrl::connection_info);
+	if (addresses == 0) {
+		return 0;
+	}
+	C callback(callgen(addresses));
+
+	size_t i = 0;
+	while(len > 0) {
+		struct ctrl::connection_info info;
+		if (recv(sock, &info, sizeof(info), MSG_WAITALL) != sizeof(info)) {
+			throw std::runtime_error(strerror(errno));
+		} else {
+			callback(&info, i++);
+			len -= sizeof(info);
+		}
+	}
+	return addresses;
 }
 
 
