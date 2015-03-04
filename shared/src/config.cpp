2a4b543e80d17524eae35a2b923085fc0d773224
James Litton <litton@cs.umd.edu>
2015-03-04 13:12:52 -0500

    permission dropping and umask, so if we just have a connector group and all users are in it, life is easier

--- shared/src/config.cpp
+++ shared/src/config.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <pwd.h>
+#include <grp.h>
 #include <boost/program_options.hpp>
 
 #include "config.hpp"
@@ -11,7 +16,42 @@ static Config g_config;
 
 namespace po = boost::program_options;
 
-int startup_setup(int argc, char * argv[]) {
+int drop_perms(const char *username, const char *group) {
+	struct passwd *pwd = getpwnam(username);
+	if (pwd == nullptr) {
+		cerr << "Username does not exist\n";
+		return 4;
+	}
+
+	struct group *grp = getgrnam(group);
+	if (grp == nullptr) {
+		cerr << "Group does not exist\n";
+		return 4;
+	}
+
+	uid_t uid = pwd->pw_uid;
+	gid_t gid = grp->gr_gid;
+
+	if (getgid() != gid) {
+		if (setgid(gid) < 0) {
+			cerr << "Could not setgid: " << strerror(errno) << endl;
+			return 8;
+		}
+	}
+
+	if (getuid() != uid) {
+		if (setuid(uid) < 0) {
+			cerr << "Could not setuid: " << strerror(errno) << endl;
+			return 8;
+		}
+	}
+
+	umask(2);
+
+	return 0;
+}
+
+int startup_setup(int argc, char * argv[], bool do_perms) {
    po::options_description desc("Options");
    desc.add_options()
       ("help", "Produce help message")
@@ -28,6 +68,15 @@ int startup_setup(int argc, char * argv[]) {
    }
 
    load_config(pvm["configfile"].as<string>().c_str());
+   if (do_perms) {
+	   int err = drop_perms((const char*) g_config.lookup("permissions.username"),
+	                        (const char*) g_config.lookup("permissions.group"));
+	   if (err) {
+		   return err;
+	   }
+   }
+   
+
    if (pvm["daemonize"].as<int>() == 1) {
       if (daemon(0, 0)) {
          cerr << "Failed to daemonize: " << strerror(errno) << endl;
